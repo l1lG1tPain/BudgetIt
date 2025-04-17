a601455ec3d7091068076c523ef205878380487d
l1lG1tPain <project.xype@mail.ru>
2025-04-17 20:57:06 +0500

    v.2.6c

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // === CONFIG ===
-const CACHE_VERSION = "budgetit-v2.6b";
-const CACHE_NAME = `budgetit-cache-v2.6b`;
+const CACHE_VERSION = "budgetit-v2.6c";
+const CACHE_NAME = `budgetit-cache-v2.6c`;
 const ASSETS = [
   "/", // Главная страница
   "/index.html",
